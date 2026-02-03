232a261a61081772d7a0915e7b075d59656d6444
aadil-1 <aadilfaisal1068@gmail.com>
2026-02-03 20:43:19 +0500

    Updated Main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include"core.h"
 int main()
 {
-	//cout<<boolalpha<<validateMove("r1bk3r/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1", "c8 b7");
+	//cout<<boolalpha<<validateMove("r1bk3r/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1", "c8 e6");
 	call();
 }//r1bk3r/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1
