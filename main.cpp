11aad92daa66b5bcfaa85a9227f6248da6a6dbe5
Bhavish Jan <bhavishjanparhyar@gmail.com>
2026-03-16 20:43:45 +0500

    bhavish is here!

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 int main()
 {
 	//cout << boolalpha << validateMove("Nrnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 1", "e7 e5");
-	
+	//Bhavish is here!	
 	call();
 }//r1bk3r/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1
 //cout<<boolalpha<<validateMove("r1bk3r/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1", "a6 b4");
